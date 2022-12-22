8d9a01f2bbc6cd23c1f0c432525b1014d92e2c3a
Adam Iverson <aciverso@gmail.com>
2022-12-22 06:58:24 -0600

    bottom tabs implemented and functioning, firebase work currently paused at non-responsive

--- App.js
+++ App.js
@@ -1,24 +1,36 @@
+import * as React from 'react';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
 import { Button, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+
 import HomeScreen from './screens/HomeScreen';
 import ProgramsScreen from './screens/ProgramsScreen';
 import PricingScreen from './screens/PricingScreen';
 import ScheduleScreen from './screens/ScheduleScreen';
-import BlogScreen from './screens/BlogScreen';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Programs" component={ProgramsScreen} />
+        <Tab.Screen name="Pricing" component={PricingScreen} />
+        <Tab.Screen name="Schedule" component={ScheduleScreen} />
+      </Tab.Navigator>
+      {/* <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Programs" component={ProgramsScreen} />
         <Stack.Screen name="Pricing" component={PricingScreen} />
         <Stack.Screen name="Schedule" component={ScheduleScreen} />
-        <Stack.Screen name="Blog" component={BlogScreen} />
-      </Stack.Navigator>
+      </Stack.Navigator> */}
     </NavigationContainer>
   );
 }
