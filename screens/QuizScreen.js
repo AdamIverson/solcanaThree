c0832883c6762952f39c46ee8c0ddced7ff33ac1
Adam Iverson <aciverso@gmail.com>
2023-02-02 14:54:28 -0600

    go back from quiz screen in place

--- screens/QuizScreen.js
+++ screens/QuizScreen.js
@@ -1,10 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const QuizScreen = () => {
+const QuizScreen = ({ navigation }) => {
   return (
     <View>
       <Text>QuizScreen</Text>
+      <Button title="go back" onPress={() =>navigation.goBack()}></Button>
     </View>
   )
 }
