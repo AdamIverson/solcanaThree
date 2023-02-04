8fe39bc55dd95ffcdecb8defd22deb3af8f77a86
Adam Iverson <aciverso@gmail.com>
2023-02-04 08:25:37 -0600

    dropdown menu installed

--- screens/QuizScreen.js
+++ screens/QuizScreen.js
@@ -1,22 +1,46 @@
-import { Button, StyleSheet, Text, View, TextInput } from 'react-native'
-import React from 'react'
+import { Button, StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import DropDownPicker from 'react-native-dropdown-picker'
 
 const QuizScreen = ({ navigation }) => {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
+  const [items, setItems] = useState([
+    {label: 'Apple', value: 'apple'},
+    {label: 'Banana', value: 'banana'}
+  ]);
   return (
     <View>
-      <Button title="go back" onPress={() =>navigation.goBack()}></Button>
-      <Text>QuizScreen</Text>
       <View>
-        <TextInput placeholder='First Name'/>
-        <TextInput placeholder='Last Name'/>
+        <Button title="go back" onPress={() => navigation.goBack()}></Button>
       </View>
       <View>
-        <TextInput placeholder='Pronouns'/>
-        <TextInput placeholder='Phone'/>
+        <Text>Which Solcana Program is right for you?</Text>
+        <Text>Take the quiz to find out</Text>
       </View>
       <View>
-        <TextInput placeholder='Email'/>
-        {/* <Picker placeholder='Dropdown' /> */}
+        <TextInput placeholder='First Name' />
+        <TextInput placeholder='Last Name' />
+      </View>
+      <View>
+        <TextInput placeholder='Pronouns' />
+        <TextInput placeholder='Phone' />
+      </View>
+      <View>
+        <TextInput placeholder='Email' />
+        <DropDownPicker
+          open={open}
+          value={value}
+          items={items}
+          setOpen={setOpen}
+          setValue={setValue}
+          setItems={setItems}
+        />
+      </View>
+      <View>
+        <TouchableOpacity>
+          <Text>Submit</Text>
+        </TouchableOpacity>
       </View>
     </View>
   )
