4b39d047e1609ea1bb7f859d2ecd119b715af913
Adam Iverson <aciverso@gmail.com>
2023-02-22 08:56:08 -0600

    schedule screen formatting started

--- screens/ScheduleScreen.js
+++ screens/ScheduleScreen.js
@@ -1,19 +1,42 @@
-import { StyleSheet, Text, View, Image} from 'react-native'
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native'
 import React from 'react'
 import ModalComponent from '../components/Modal'
+import { FlatList } from 'react-native-gesture-handler'
+
+// data sources
+const classesMF = [
+  { title: '6am', id: 1 },
+  { title: '7am', id: 2 },
+  { title: '9am', id: 3 },
+  { title: '12pm', id: 4 },
+  { title: '4:30pm', id: 5 },
+  { title: '5:30pm', id: 6 },
+  { title: '6:30pm', id: 7 },
+];
+
+const classesSaturday = [
+  { title: '8am', id: 1 },
+  { title: '9am', id: 2 },
+  { title: '10am', id: 3 },
+]
+
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
 
 const ScheduleScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text>ScheduleScreen</Text>
-      <View style={styles.image}>
-        <Image
-          style={styles.image}
-          source={require('../assets/schedule.png')}
-        />
+    <SafeAreaView
+      style={styles.container}
+    >
+      <View>
+        <Text style={styles.header}>
+          Group Fitness Classes
+        </Text>
       </View>
-      <ModalComponent />
-    </View>
+    </SafeAreaView>
   )
 }
 
@@ -22,13 +45,23 @@ export default ScheduleScreen
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ff854d',
+    backgroundColor: 'orange',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  image: {
+  header: {
+    fontSize: 25,
+    justifyContent: 'center'
+  },
+  list: {
+    borderColor: 'red',
+    borderWidth: 5,
+  },
+  classRow: {
     flex: 1,
-    width: 400,
-    height: 200,
+    flexDirection: 'row'
+  },
+  classUnit: {
+    width: 10,
   }
 })
