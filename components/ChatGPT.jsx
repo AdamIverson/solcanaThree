b38217f73f2ba25a67c7fac86d2c84a899b9462f
Adam Iverson <aciverso@gmail.com>
2023-03-08 12:16:10 -0600

    openai installed

---
+++ components/ChatGPT.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ChatGPT = () => {
+  return (
+    <View>
+      <Text>ChatGPT</Text>
+    </View>
+  )
+}
+
+export default ChatGPT
+
+const styles = StyleSheet.create({})
