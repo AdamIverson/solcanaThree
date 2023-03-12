0fc31b60f2e08c0c563e2e7f74d496dc45beebdf
Adam Iverson <aciverso@gmail.com>
2023-03-12 12:50:04 -0500

    headerTitle unique for each Stack.Screen

--- App.js
+++ App.js
@@ -1,38 +1,15 @@
-import React from 'react';
-// import { MaterialCommunityIcons } from '@expo/vector-icons';
-
+import React, { useState } from 'react';
 import { Text, StyleSheet, Linking } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-// import { createStackNavigator } from '@react-navigation/stack';
-// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Main from './components/Main';
 import QuizScreen from './screens/QuizScreen';
-// import HomeScreen from './screens/HomeScreen';
-// import ProgramsScreen from './screens/ProgramsScreen';
-// import PricingScreen from './screens/PricingScreen';
-// import ScheduleScreen from './screens/ScheduleScreen';
-
-// import ModalComponent from './components/Modal';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import EmPowerScreen from './screens/EmPowerScreen';
 import PersonalTrainingScreen from './screens/PersonalTrainingScreen';
 
 const Stack = createNativeStackNavigator();
-// const Tab = createBottomTabNavigator();
-
-
-// function QuizScreen({ navigation }) {
-//   return (
-//     <Stack.Navigator>
-//       <Stack.Screen
-//         name="Main"
-//         component={Main}
-//          />
-//     </Stack.Navigator>
-//   )
-// }
 
 function phoneCall() {
   const url = 'tel://6128080252';
@@ -41,6 +18,8 @@ function phoneCall() {
 
 
 export default function App() {
+  const [appTitle, setAppTitle] = useState('Solcana Fitness')
+
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -76,12 +55,26 @@ export default function App() {
         tabBarInactiveTintColor: '#fff'
       })}
       >
-        <Stack.Screen name='Main' component={Main}
-        options={({ route }) => ({ title: route.name })}
+        <Stack.Screen 
+          name={appTitle} 
+          component={Main}
+          options={({ route }) => ({ title: route.name })}
         />
-        <Stack.Screen name='QuizScreen' component={QuizScreen} />
-        <Stack.Screen name='EmPowerScreen' component={EmPowerScreen} />
-        <Stack.Screen name='PersonalTrainingScreen' component={PersonalTrainingScreen} />
+        <Stack.Screen 
+          name='QuizScreen' 
+          component={QuizScreen}
+          options={() => ({ headerTitle: "Go Fitness!" })}
+          />
+        <Stack.Screen 
+          name='EmPowerScreen' 
+          component={EmPowerScreen}
+          options={() => ({ headerTitle: "EmPower!" })}
+          />
+        <Stack.Screen 
+          name='PersonalTrainingScreen' 
+          component={PersonalTrainingScreen}
+          options={() => ({ headerTitle: "Personal Training!" })}
+          />
       </Stack.Navigator>
     </NavigationContainer>
   );
